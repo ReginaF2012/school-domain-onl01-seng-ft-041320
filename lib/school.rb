e14c31ce462c377f704ddcb7d522483bfda04237
Regina Furness <skritschy@gmail.com>
2020-04-19 23:14:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,10 @@
 class School
+  attr_reader :name, :roster
   def initialize(name)
     @name = name
     @roster = {}
   end
-
-  def roster
-    @roster
-  end
-
+  
   def add_student(name, grade)
     if @roster[grade] == nil
       @roster[grade] = [name]
